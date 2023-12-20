5dfd44805a5ce1761ba68d43a4453fc6f78d5796
Chris Phillips <chris.phillips@canarie.ca>
2023-12-20 16:35:28 -0500

    Update CAF_entityCategories.ps1 to improve R&S alignment
    - [x] resolved problem where CAF entities releasing assurance for R&S entities

--- ADFSToolkit/config/federation/CAF/CAF_entityCategories.ps1
+++ ADFSToolkit/config/federation/CAF/CAF_entityCategories.ps1
@@ -18,18 +18,35 @@ function Import-ADFSTkIssuanceTransformRuleCategoriesFromFederation {
     ### CAF Entity Categories ###
     ################################
 
-    # v1.0
-    # CAF does not have any specific federation level entity category elements at this time.
+    # v2.2.1 
+    # CAF adjusts the R&S entity category to be more strict to ensure accurate R&S ranking at https://release-check.edugain.org
     # for reference as to what can appear in this file please see ../SWAMID/SWAMID_entityCategories.ps1
 
+    ### CAF Entity Category Research and Education
 
-    $IssuanceTransformRuleCategories = @{}
+ # Special note: if you require edupersonAssurance, please update get-ADFSTkLocalManualSpSettings.ps1
+ # to augment and add that extra value in for that specific service
 
+
+ $IssuanceTransformRuleCategoriesCustomized = [Ordered]@{}
+
+    $TransformRules = [Ordered]@{}
+
+    $TransformRules.eduPersonPrincipalName = $Global:ADFSTkAllTransformRules.eduPersonPrincipalName
+    $TransformRules.mail = $Global:ADFSTkAllTransformRules.mail
+    $TransformRules.displayName = $Global:ADFSTkAllTransformRules.displayName
+    $TransformRules.givenName = $Global:ADFSTkAllTransformRules.givenName
+    $TransformRules.sn = $Global:ADFSTkAllTransformRules.sn
+    $TransformRules.eduPersonScopedAffiliation = $Global:ADFSTkAllTransformRules.eduPersonScopedAffiliation
+
+    
+    $IssuanceTransformRuleCategoriesCustomized.Add("http://refeds.org/category/research-and-scholarship", $TransformRules)
+ 
+
+ ###
+
+    #return the adjusted entity categories to be processed accordingly
   
-    ###
+   $IssuanceTransformRuleCategoriesCustomized 
 
-  
-    ###
-
-    $IssuanceTransformRuleCategories
 }
